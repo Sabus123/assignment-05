724cf6a26d46b64410fadf648b5bbfee9d42d53f
Sabus123 <msm01777946259@gmail.com>
2025-03-03 08:16:57 +0600

    this is modified code

--- js/toggle.js
+++ js/toggle.js
@@ -3,9 +3,18 @@ document.getElementById("btn-blog").addEventListener("click", function () {
 });
 
 
+document.getElementById('back-btn').addEventListener("click", function() {
+  window.location.href = '/index.html';  // Redirect to the main page (index.html)
+});
+
 
 
 // initially activity loge massage not seeing.
 document.getElementById('complete-btn').addEventListener("click", function(){
   document.getElementById('initial-btn').style.display = "none";
 })
+
+
+
+// back the webpage main page
+
